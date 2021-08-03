929deee7106a2ab261549695f1468decfcb75a17
DESKTOP-H4MNR9D\Seidai Hanashiro <c21017@std.it-college.ac.jp>
2021-08-03 15:27:54 +0900

    不要スクリプトの削除+他の細かい作業

--- Season2_test/Assets/SeidaiScripts/BlackHole.cs
+++ Season2_test/Assets/SeidaiScripts/BlackHole.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class BlackHole : MonoBehaviour
 {
     public GameObject BlackHolePrefab;
-    public float shotForceLight;
-    public float shotTorque;
-    void Update()//左クリックで軽い弾発射
+    void Update()//左クリックでブラックホール発射
     {
         if (Input.GetMouseButtonDown(0))
 
@@ -15,13 +13,9 @@ public class BlackHole : MonoBehaviour
     }
     public void Shot()
     {
-        //if (costManager.GetCostAmount() < 10) return; //コスト不足なら発射不可,ここのコストは弾ごとでちゃんと一致させること
         float PZ = this.transform.position.z;
         PZ += 2;
         GameObject light = (GameObject)Instantiate(BlackHolePrefab, new Vector3(0, 5, PZ),
         Quaternion.Euler(0, 0, 0));
-        /*Rigidbody blackRigidBody = light.GetComponent<Rigidbody>();
-        blackRigidBody.AddForce(transform.forward * shotForceLight * 100);
-        blackRigidBody.AddTorque(new Vector3(0, shotTorque, 0));*/
     }
 }
--- Season2_test/Assets/SeidaiScripts/BlackHoleDestroy.cs
+++ Season2_test/Assets/SeidaiScripts/BlackHoleDestroy.cs
@@ -4,23 +4,12 @@ using UnityEngine;
 
 public class BlackHoleDestroy : MonoBehaviour
 {
-    /*public GameObject effectPrefab;
-    public Vector3 effectRotation;*/
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Gomi")//床のタグ(wall)を検知し、自身を消滅させスコア加算。スコアはUnity上で設定可能。
+        if (other.gameObject.tag == "Gomi")
         {
             //scoreManager.AddScore(score);
             Destroy(this.gameObject);
         }
-        //エフェクトの出現(エフェクト内SEも仕込めば音鳴らせます)
-        //標的によってエフェクト変えたかったら違うプレファブを用意すること。
-        /*if (effectPrefab != null)
-        {
-            Instantiate(
-            effectPrefab,
-            this.transform.position,
-            Quaternion.Euler(effectRotation));
-        }*/
     }
 }
